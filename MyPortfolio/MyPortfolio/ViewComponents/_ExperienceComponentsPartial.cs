c7e03e96452b2d01357d7a07e90d6b498fb37b4c
Tolu00 <rusentolunay@gmail.com>
2024-08-18 13:01:55 +0300

    MyPorfolio
    Öğrendiklerim
    Bu proje sayesinde aşağıdaki konularda bilgi ve deneyim kazandım:
    
    Asp.Net Core kullanarak web uygulamaları geliştirme
    SQL veritabanı entegrasyonu ve yönetimi
    HTML, CSS, Bootstrap ve JavaScript ile kullanıcı arayüzü tasarımı
    MVC (Model-View-Controller) mimarisi
    View Componentler ile modüler yapı oluşturma
    Code First yaklaşımı ve Migration işlemleri

---
+++ MyPortfolio/MyPortfolio/ViewComponents/_ExperienceComponentsPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _ExperienceComponentsPartial : ViewComponent
+    {
+        MyPortfolioContext MyPortfolioContext = new MyPortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            var values = MyPortfolioContext.Experiences.ToList();
+            return View(values);
+        }
+    }
+}
